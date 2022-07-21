66e8ba16caf5e3f5d5852aa96c8c1fd2a9703010
Venthan <yangwensen@outlook.com>
2022-07-21 20:58:39 +0800

    make it better

--- config.sh
+++ config.sh
@@ -1,3 +1,23 @@
+cd ~
+apt update
+apt install --y build-essential libtool gcc automake autoconf make
+apt install -y wget
+
+git https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git
+wget http://nginx.org/download/nginx-1.21.6.tar.gz
+tar -zxvf nginx-1.21.6.tar.gz
+cd nginx-1.21.6.tar.gz
+./configure --sbin-path=/usr/local/nginx/sbin/nginx \
+--conf-path=/etc/nginx/conf.d/nginx.conf \
+--with-http_stub_status_module \
+--with-http_ssl_module \
+--with-http_v2_module \
+--add-module=/root/ngx_http_substitutions_filter_module
+
+make && make install
+alias nginx=/usr/local/nginx/sbin/nginx
+# nginx -V
+
 #!/bin/bash
 set -e
 set -o pipefail
